fdb542a62d79db3cdfbeca794a8ae9abd57b99b3
Regis Rufino <regis.rfnrodrigues@gmail.com>
2020-11-26 13:29:42 -0300

    add cors

--- src/app.js
+++ src/app.js
@@ -5,15 +5,24 @@ const db = require('./database/db')
 const authRoute = require('./routes/auth')
 require('dotenv').config()
 
+var corsOptions = {
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204,
+    "allowedHeaders": "Content-Type,Authorization"
+}
+
+app.use(cors(corsOptions))
 // routes
 app.use('/api/v1/user', authRoute)
 db.connect(`${process.env.DATABASE_API}/authApp_users_test`)
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     return res.status(200).json({
         message: "Welcome Home"
     })
-}) 
+})
 
 var port = process.env.PORT || 5000
 app.listen(port)
