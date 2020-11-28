4b56237bf3cb1473c8e90ad3e033bc72fc02de16
Regis Rufino <regis.rfnrodrigues@gmail.com>
2020-11-28 00:23:21 -0300

    Tested post

--- src/test/createUserHttp_test.js
+++ src/test/createUserHttp_test.js
@@ -20,7 +20,28 @@ describe('/POST user', () => {
                 // console.log(res)
                 res.should.have.status(200);
                 res.body.should.be.a('object');
-                res.body.should.have.property('message').eql('Book successfully added!');
+                res.body.should.have.property('user');
+                res.body.should.have.property('message').eql('OK');
+                done();
+            })
+    })
+    it('it should NOT post a user', (done) => {
+        let user = {
+            name: undefined,
+            password: "123456",
+            email: "joe@gmail.com",
+        }
+        chai.request(server)
+            .post('/api/v1/user/register')
+            .send(user)
+            .end((err, res) => {
+                // console.log(res)
+                res.should.have.status(400);
+                res.body.should.be.a('object');
+                res.body.should.not.have.property('user');
+                res.body.should.have.property('message').eql('Not OK');
+                res.body.should.have.property('error').eql({name:{message:"Name is required"}})
+                done();
             })
     })
 })
